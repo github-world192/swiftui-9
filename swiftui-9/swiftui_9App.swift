d54f456b91eb511c78a292fb11f4bcb4ed1c8c5f
github-world192 <dillen99999999@gmail.com>
2023-10-18 19:12:37 -0700

    Initial Commit

---
+++ swiftui-9/swiftui_9App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_9App.swift
+//  swiftui-9
+//
+//  Created by llll on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
